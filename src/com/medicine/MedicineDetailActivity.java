1bcb563eaee5a851519308076aa79f70601e95a1
yang_l@yeah.net <yang.yang5101@gmail.com>
2014-03-21 23:35:59 +0800

    detail_part1

---
+++ src/com/medicine/MedicineDetailActivity.java
@@ -0,0 +1,14 @@
+package com.medicine;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MedicineDetailActivity extends Activity{
+
+	@Override
+	public void onCreate(Bundle b){
+		super.onCreate(b);
+	}
+	
+	
+}
