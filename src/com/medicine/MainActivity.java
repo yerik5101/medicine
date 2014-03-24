e0f52613ce47ff0399a5784eeb2ee57d0f45627a
yang_l@yeah.net <yang.yang5101@gmail.com>
2014-03-25 01:28:47 +0800

    构造数据

--- src/com/medicine/MainActivity.java
+++ src/com/medicine/MainActivity.java
@@ -5,8 +5,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -16,16 +19,18 @@ import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
 
 import com.medicine.bean.MedicineBean;
+import com.medicine.dialog.DialogUtil;
 import com.medicine.util.JsonUtil;
 import com.medicine.util.StringUtil;
 
 public class MainActivity extends Activity {
-
+	private static final int FILL_DATA = 0x0001;
 	private ListView list_view;
 	private List<MedicineBean> list,searchList;
 	private SearchText search_text;
 	private MedicineAdapter medicineAdapter;
 	private View conver_nodata;
+	private Dialog dialog;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -73,6 +78,27 @@ public class MainActivity extends Activity {
 	}
 	
 	private void initData(){
+		dialog = DialogUtil.createLoadingDialog(this, "正在加载数据...");
+		dialog.show();
+		new Thread(){
+			@Override
+			public void run(){
+				initListData();
+			}
+		}.start();
+	}
+	
+	Handler myHandler = new Handler(){
+		
+		@Override
+		public void handleMessage(Message msg){
+			if(msg!=null&&msg.what==FILL_DATA){
+				fillData();
+			}
+		}
+	};
+	
+	private void initListData(){
 		list = new ArrayList<MedicineBean>();
 		String data1 = getString(R.string.data1);
 		MedicineBean mb1 = JsonUtil.parseJson(data1, MedicineBean.class);
@@ -90,6 +116,56 @@ public class MainActivity extends Activity {
 		MedicineBean mb7 = JsonUtil.parseJson(data7, MedicineBean.class);
 		String data8 = getString(R.string.data8);
 		MedicineBean mb8 = JsonUtil.parseJson(data8, MedicineBean.class);
+		String data9 = getString(R.string.data9);
+		MedicineBean mb9 = JsonUtil.parseJson(data9, MedicineBean.class);
+		System.out.println("json parse 9");
+		String data10 = getString(R.string.data10);
+		MedicineBean mb10 = JsonUtil.parseJson(data10, MedicineBean.class);
+		System.out.println("json parse 10");
+		String data11 = getString(R.string.data11);
+		MedicineBean mb11 = JsonUtil.parseJson(data11, MedicineBean.class);
+		System.out.println("json parse 11");
+		String data12 = getString(R.string.data12);
+		MedicineBean mb12 = JsonUtil.parseJson(data12, MedicineBean.class);
+		System.out.println("json parse 12");
+		String data13 = getString(R.string.data13);
+		MedicineBean mb13 = JsonUtil.parseJson(data13, MedicineBean.class);
+		System.out.println("json parse 13");
+		String data14 = getString(R.string.data14);
+		MedicineBean mb14 = JsonUtil.parseJson(data14, MedicineBean.class);
+		System.out.println("json parse 14");
+		String data15 = getString(R.string.data15);
+		MedicineBean mb15 = JsonUtil.parseJson(data14, MedicineBean.class);
+		String data16 = getString(R.string.data15);
+		MedicineBean mb16 = JsonUtil.parseJson(data16, MedicineBean.class);
+		String data17 = getString(R.string.data17);
+		MedicineBean mb17 = JsonUtil.parseJson(data17, MedicineBean.class);
+		String data18 = getString(R.string.data18);
+		MedicineBean mb18 = JsonUtil.parseJson(data18, MedicineBean.class);
+		String data19 = getString(R.string.data19);
+		MedicineBean mb19 = JsonUtil.parseJson(data19, MedicineBean.class);
+		String data20 = getString(R.string.data20);
+		MedicineBean mb20 = JsonUtil.parseJson(data20, MedicineBean.class);
+		String data21 = getString(R.string.data21);
+		MedicineBean mb21 = JsonUtil.parseJson(data21, MedicineBean.class);
+		String data22 = getString(R.string.data22);
+		MedicineBean mb22 = JsonUtil.parseJson(data22, MedicineBean.class);
+		String data23 = getString(R.string.data23);
+		MedicineBean mb23 = JsonUtil.parseJson(data23, MedicineBean.class);
+		String data24 = getString(R.string.data24);
+		MedicineBean mb24 = JsonUtil.parseJson(data24, MedicineBean.class);
+		String data25 = getString(R.string.data25);
+		MedicineBean mb25 = JsonUtil.parseJson(data25, MedicineBean.class);
+		String data26 = getString(R.string.data26);
+		MedicineBean mb26 = JsonUtil.parseJson(data26, MedicineBean.class);
+		String data27 = getString(R.string.data27);
+		MedicineBean mb27 = JsonUtil.parseJson(data27, MedicineBean.class);
+		String data28 = getString(R.string.data28);
+		MedicineBean mb28 = JsonUtil.parseJson(data28, MedicineBean.class);
+		String data29 = getString(R.string.data29);
+		MedicineBean mb29 = JsonUtil.parseJson(data29, MedicineBean.class);
+		String data30 = getString(R.string.data30);
+		MedicineBean mb30 = JsonUtil.parseJson(data30, MedicineBean.class);
 		list.add(mb1);
 		list.add(mb2);
 		list.add(mb3);
@@ -98,7 +174,37 @@ public class MainActivity extends Activity {
 		list.add(mb6);
 		list.add(mb7);
 		list.add(mb8);
-		fillData();
+		list.add(mb9);
+		list.add(mb10);
+		list.add(mb11);
+		list.add(mb12);
+		list.add(mb13);
+		list.add(mb14);
+		list.add(mb15);
+		list.add(mb16);
+		list.add(mb17);
+		list.add(mb18);
+		list.add(mb19);
+		list.add(mb20);
+		list.add(mb21);
+		list.add(mb22);
+		list.add(mb23);
+		list.add(mb24);
+		list.add(mb25);
+		list.add(mb26);
+		list.add(mb27);
+		list.add(mb28);
+		list.add(mb29);
+		list.add(mb30);
+		
+		
+		sendFillDataMessage();
+	}
+	
+	private void sendFillDataMessage(){
+		Message msg = new Message();
+		msg.what = FILL_DATA;
+		myHandler.sendMessage(msg);
 	}
 	
 	private void fillData(){
@@ -121,6 +227,9 @@ public class MainActivity extends Activity {
 			}
 			
 		});
+		if(dialog!=null){
+			dialog.dismiss();
+		}
 	}
 	
 	public interface WaterChangedCallback{
@@ -149,8 +258,13 @@ public class MainActivity extends Activity {
 	
 	private void cannelSearch(){
 		search_text.getEt_searchtext_search().setText("");
+		hideSoftInput();
+	}
+	
+	private void hideSoftInput(){
+		search_text.getEt_searchtext_search().clearFocus();
 		InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);   
-		imm.toggleSoftInput(0, InputMethodManager.HIDE_NOT_ALWAYS);
+		imm.hideSoftInputFromWindow(search_text.getEt_searchtext_search().getWindowToken(), 0);
 	}
 	
 	@Override  
@@ -164,4 +278,5 @@ public class MainActivity extends Activity {
         } else  
             return super.onKeyDown(keyCode, event);  
     }
+	
 }
